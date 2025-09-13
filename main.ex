8516b1d482334a136c9ec06b17b3a1ef8d097e61
unknown <ezekuiel100@gmail.com>
2025-09-13 19:48:36 -0300

    keyword list

--- main.ex
+++ main.ex
@@ -5,7 +5,13 @@ stats =
   |> Enum.reduce(%{}, fn line, acc ->
     [location, temp] = String.split(line, ";")
 
-    Map.put(acc, "#{location}", temp)
+    if acc[location] do
+      value = Map.get(acc, location)
+      newValue = [max: temp, min: temp, count: value[:count] + 1]
+      Map.put(acc, "#{location}", newValue)
+    else
+      Map.put(acc, "#{location}", max: temp, min: temp, count: 1)
+    end
   end)
 
 IO.inspect(stats)
